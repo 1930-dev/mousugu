89c0e51fb6a9d02911a4e3a76b935c64213984cd
Agu Rodríguez <me@agu.uy>
2026-05-25 17:27:21 -0300

    Initial Commit

---
+++ MenuBarCalendar/MenuBarCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarCalendarApp.swift
+//  MenuBarCalendar
+//
+//  Created by Agu Rodríguez on 25/5/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
